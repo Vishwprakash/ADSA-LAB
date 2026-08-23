bf450a51ca74c651989224978993c66aa4f19b2b
Vishwprakash <140044200+Vishwprakash@users.noreply.github.com>
2026-08-23 12:00:45 +0530

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted array is: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main()
+{
+    int arr[] = {112, 23, 34, 6, 7, 0, 12};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // function calling
+    bubble_sort(arr, size);
+    return 0;
+}
